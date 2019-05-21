bf73df807ac791ba7800e6163ef853b0ef57d265
Le Ngoc Trong <ntrong0603.dgt@gmail.com>
2019-05-21 15:36:38 +0700

    add font-end products, products add, lib ckeditor

--- models/products.model.js
+++ models/products.model.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 // khai bao nhung field co trong obj products ( hay bang products)
 const productsSchema = new mongoose.Schema({
-    stt: { type: Number, default: 1},
+    serial: { type: Number, default: 1},
     name: { type: String, required: true},
     avata: String,
     price: { type: String, default: 0},
     count: Number,
-    decription: String,
+    sold: Number,
+    description: String,
     content: String,
+    view: Number,
     catalog_id: { type: String, required: true},
     hidden: {type: Boolean, default: true},
     day_create: { type: Date, default: Date.now },
